200985f705f661cf45d336ee4a6c8d591e438b11
shewa12 <shewa12kpi@gmail.com>
2022-07-23 22:36:32 +0600

    new: field options creation added

--- includes/FormBuilder/FormClient.php
+++ includes/FormBuilder/FormClient.php
@@ -21,6 +21,7 @@ class FormClient {
 
 	protected $form_builder;
 	protected $form_field_builder;
+	protected $field_options;
 
 	/**
 	 * Init props & register hooks
@@ -31,6 +32,7 @@ class FormClient {
 		// Initialize props.
 		$this->form_builder       = FormBuilder::create( 'Form' );
 		$this->form_field_builder = FormBuilder::create( 'FormField' );
+		$this->field_options      = FormBuilder::create( 'FieldOptions' );
 
 		add_action( 'wp_ajax_ep_single_multiple_question_create', array( $this, 'single_multiple_question_create' ) );
 
@@ -47,14 +49,39 @@ class FormClient {
 	public function single_multiple_question_create() {
 		Utilities::verify_nonce();
 
-		$post     = $_POST; //phpcs:ignore WordPress.Security.NonceVerification.Missing
-		$request  = array(
+		$post    = $_POST; //phpcs:ignore WordPress.Security.NonceVerification.Missing
+		$request = array(
 			'poll_id'     => $post['poll-id'],
 			'field_label' => $post['ep-field-label'],
 			'field_type'  => $post['ep-field-type'],
 		);
-		$response = $this->form_field_builder->create( $request );
-		return $response ? wp_send_json_success( $response ) : wp_send_json_error( __( 'Field creation failed!', 'easy-poll' ) );
+
+		$response      = false;
+		$error_message = __( 'Field creation failed!', 'easy-poll' );
+		// Get field id that is inserted.
+		$field_id = $this->form_field_builder->create( $request );
+		if ( $field_id ) {
+			$options = array();
+			// Prepare options.
+			foreach ( $post['ep-field-option'] as $option ) {
+				if ( '' === $option ) {
+					continue;
+				}
+				$data = array(
+					'field_id'     => $field_id,
+					'option_label' => $option,
+				);
+				array_push( $options, $data );
+			}
+			// Insert field options.
+			if ( count( $options ) ) {
+				$response = $this->field_options->create_multiple( $options );
+				if ( ! $response ) {
+					$error_message = __( 'Field options creation failed!', 'easy-poll' );
+				}
+			}
+		}
+		return $response ? wp_send_json_success( $response ) : wp_send_json_error( $error_message );
 	}
 
 	/**
@@ -67,7 +94,7 @@ class FormClient {
 	public function input_textarea_question_create() {
 		Utilities::verify_nonce();
 
-		$post = $_POST; //phpcs:ignore WordPress.Security.NonceVerification.Missing
+		$post    = $_POST; //phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$request = array();
 
 		foreach ( $post['ep-field-label'] as $key => $field ) {
@@ -76,9 +103,9 @@ class FormClient {
 			}
 
 			$data = array(
-				'poll_id'         => $post['poll-id'],
-				'field_label'     => $field,
-				'field_type'      => $post['ep-field-type'][ $key ],
+				'poll_id'     => $post['poll-id'],
+				'field_label' => $field,
+				'field_type'  => $post['ep-field-type'][ $key ],
 			);
 			array_push( $request, $data );
 		}
@@ -87,7 +114,7 @@ class FormClient {
 			$response = $this->form_field_builder->create_multiple( $request );
 			return $response ? wp_send_json_success( $response ) : wp_send_json_error( __( 'Field creation failed!', 'easy-poll' ) );
 		} else {
-			return wp_send_json_error( __( 'Field not exist!', 'easy-poll') );
+			return wp_send_json_error( __( 'Field not exist!', 'easy-poll' ) );
 		}
 	}
 
