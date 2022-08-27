919ef41ae70ab0e230b68d9b631a8a2adf4f15a8
shewa12 <shewa12kpi@gmail.com>
2022-08-27 17:02:23 +0600

    new: rewrite rules added to prevent 404 error

---
+++ includes/Settings/RewirteRules.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Manage rewrite rules
+ *
+ * @since v1.0.1
+ *
+ * @package EasyPoll\Settings
+ */
+
+namespace EasyPoll\Settings;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Do stuff rewrite or flush rules
+ */
+class RewriteRules {
+
+	/**
+	 * Register hook
+	 *
+	 * @since v1.0.1
+	 */
+	public function __construct() {
+		add_action( 'init', __CLASS__ . '::flush_rewrite_rules' );
+	}
+
+	/**
+	 * Check if permalink update is required, if so
+	 * then update.
+	 *
+	 * @since v1.0.1
+	 *
+	 * @return void
+	 */
+	public static function flush_rewrite_rules(): void {
+		if ( get_option( Options::REQUIRE_PERMALINK_UPDATE, true ) ) {
+			flush_rewrite_rules();
+			update_option( Options::REQUIRE_PERMALINK_UPDATE, false );
+		}
+	}
+}
