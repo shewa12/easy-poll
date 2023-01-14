332bdd7c8b07d425108f4f7afd81c2cb59c40268
shewa12 <shewa12kpi@gmail.com>
2023-01-15 00:27:38 +0600

    Update: Hooks added on report page

--- views/report/report-overview.php
+++ views/report/report-overview.php
@@ -11,6 +11,7 @@ use EasyPoll\Report\Report;
 
 $statistics = Report::get_poll_statistics();
 ?>
+<?php do_action( 'ep_before_polls_overview' ); ?>
 <div class="ep-card ep-p-10">
 	<h3>
 		<?php esc_html_e( 'Polls Overview', 'easy-poll' ); ?>
@@ -42,15 +43,23 @@ $statistics = Report::get_poll_statistics();
 		</div>
 	</div>
 	<!-- active polls chart  -->
-	<div class="ep-row ep-active-polls-chart ep-mt-20">
+	<div class="ep-row ep-active-polls-chart ep-mt-20 ep-justify-between ep-align-center">
 		<h3>
-			<?php esc_html_e( 'Last 7 Days Report of Active Polls', 'easy-poll' ); ?>
+			<?php esc_html_e( 'Statistics of Active Polls', 'easy-poll' ); ?>
 		</h3>
+		<div>
+			<?php
+			$report_days = '<h3> ' . __( 'Last 7 Days Report', 'easy-poll' ) . ' <h3>';
+			// @ep_report_days filter-hook.
+			echo wp_kses_post( apply_filters( 'ep_report_days', $report_days ) );
+			?>
+		</div>
 	</div>
 	<div class="ep-row">
 		<div id="ep-loading-msg"></div>
 		<canvas id="ep-active-polls-chart"></canvas>
 	</div>
+	<?php do_action( 'ep_after_active_polls_chart' ); ?>
 	<!-- active polls chart end -->
 </div>
 
