e6f158c0ab372c824f32ab26e8bcb5825a85a928
shewa12 <shewa12kpi@gmail.com>
2023-01-20 19:16:34 +0600

    Fix: No votes bar showing full width & 100% votes not showing bar radius on the right side of bar

--- views/report/progress-bar.php
+++ views/report/progress-bar.php
@@ -15,10 +15,11 @@
 	if ( $question !== $statistic->field_label ) {
 		continue;
 	}
+	$progress = (int) $statistic->percentage;
 	?>
 	<div class="ep-d-flex ep-progress-bar-wrapper">
 		<div class="ep-progress-bar ep-mb-10">
-			<div class="ep-progress" style="width: <?php echo esc_attr( $statistic->percentage . '%' ); ?>">
+			<div class="ep-progress ep-progress-<?php echo esc_attr( $progress ); ?>" style="width: <?php echo (int) esc_attr( $progress ) . '%'; ?>">
 			</div>
 			<span class="ep-progress-text">
 				<?php echo esc_html( $statistic->option_label ); ?>
